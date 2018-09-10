ae1d257e51c5966ee2f6385a423cc87984e2db74
Eric <eric322000@gmail.com>
2018-09-10 17:08:45 -0600

    Removed hard code and added Hello World

--- Game_Fall_Eric_Casper/Assets/Scripts/CharacterMove.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/CharacterMove.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour {
 
 	public int MoveSpeed;
-	public float JumpHeight = 15;
+	public float JumpHeight;
 
 	// Use this for initialization
 	void Start () {
-		
+		print("Hello World");
 	}
 	
 	// Update is called once per frame
