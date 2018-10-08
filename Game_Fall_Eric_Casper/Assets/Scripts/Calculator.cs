1e25471bdb19ad70bba0a67149e3dc5a2e4b17db
Eric <eric322000@gmail.com>
2018-10-08 16:07:57 -0600

    Rewritten calculator function.

---
+++ Game_Fall_Eric_Casper/Assets/Scripts/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour {
+
+	void Start () {
+		ScoreCalc = GetComponent<Text>();
+	}
+
+	//Calculation code
+	public void Add(int FirstNumber, int SecondNumber){
+		Total = FirstNumber + SecondNumber;
+		print(Total);
+
+		Total = FirstNumber - SecondNumber;
+		print(Total);
+
+		Total = FirstNumber * SecondNumber;
+		print(Total);
+
+		Total = FirstNumber / SecondNumber;
+		print(Total);
+
+	}
+	
+}
