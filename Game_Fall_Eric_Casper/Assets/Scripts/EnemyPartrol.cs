5daf884ffc7e7daa37268f3e73508317a985b5d8
Eric <eric322000@gmail.com>
2018-10-15 16:28:47 -0600

    Put in code for Enemy patrol.

--- Game_Fall_Eric_Casper/Assets/Scripts/EnemyPartrol.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/EnemyPartrol.cs
@@ -21,6 +21,22 @@ public class EnemyPartrol : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall)
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall)
+
+		// Turn around at Wall or Edge
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-0.2f,0.2f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>(). velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(0.2f,0.2f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>(). velocity.);
+		}
 	}
 }
