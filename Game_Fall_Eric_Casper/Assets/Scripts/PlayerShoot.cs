4b2380a7fca873e60850323299111edd5f09d888
Eric <eric322000@gmail.com>
2018-10-22 16:50:58 -0600

    Put in projectile function in PC and added new script for projectiles.

--- Game_Fall_Eric_Casper/Assets/Scripts/PlayerShoot.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/PlayerShoot.cs
@@ -8,10 +8,10 @@ public class PlayerShoot : MonoBehaviour {
 	public GameObject Projectile;
 
 	void Start(){
-		Projectile = GameObject.Find("Projectile")
+		Projectile = GameObject.Find("Projectile");
 	}
 	
-	// Update is called once per frame
+	// Able to shoot
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.RightControl))
 			Instantiate(Projectile,FirePoint.position, FirePoint.rotation);
---
+++ Game_Fall_Eric_Casper/Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float Speed;
+	public Rigidbody2D PC;
+
+	public GameObject EnemyDeath;
+
+	public GameObject ProjectileParticle;
+
+	public int PointsForKill;
+
+	void Start () {
+
+		if(PC.transform.localScale.x < 0)
+			Speed = -Speed;
+
+
+	}
+
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Enemy"){
+			
+		}
+	}
+}
