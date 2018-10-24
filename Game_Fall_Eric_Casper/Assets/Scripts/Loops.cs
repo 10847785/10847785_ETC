11ad13505de7fe985b5115374287296da34afe0f
Eric <eric322000@gmail.com>
2018-10-24 16:36:30 -0600

    Added while function to the looping script.

--- Game_Fall_Eric_Casper/Assets/Scripts/Loops.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/Loops.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour {
 
-
+	public int Num = 40;
 
 	// Use this for initialization
 	void Start () {
-		for(int i=0; i <= 100; i++){
+		// for(int i=0; i <= 100; i++){
 
-			print(i + " Ants Marching");
+		// 	print(i + " Ants Marching");
 
-		}
+		// }
 		
+		while(Num > 0){
+			print("Countdown" + Num);
+			Num --;
+		}
 	}
 	
 	// Update is called once per frame
