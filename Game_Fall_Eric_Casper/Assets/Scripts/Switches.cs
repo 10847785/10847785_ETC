3aa5aec186b6db1a11f69f59216ab935eb77e858
Eric Casper <eric322000@gmail.com>
2018-10-29 16:45:37 -0600

    Put in name variable in switches script

--- Game_Fall_Eric_Casper/Assets/Scripts/Switches.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/Switches.cs
@@ -5,41 +5,65 @@ using UnityEngine;
 public class Switches : MonoBehaviour {
 
 	public int num;
+	public string name;
 
 
 	// Use this for initialization
 	void Start () {
 		
-		if(num == 10){
-			print("Hurray you picked ten!");
-		}
+	// 	if(num == 10){
+	// 		print("Hurray you picked ten!");
+	// 	}
 		
-		else
-			print("Boo! You didn't pick ten!");
+	// 	else
+	// 		print("Boo! You didn't pick ten!");
 		
 
-		switch(num){
+	// 	switch(num){
 
-			case 1:
-				print("You picked" + num );
-			break;
+	// 		case 1:
+	// 			print("You picked" + num );
+	// 		break;
 
-			case 3:
-				print("You picked" + num );
-			break;
+	// 		case 3:
+	// 			print("You picked" + num );
+	// 		break;
 
-			case 6:
-				print("You picked" + num );
-			break;
+	// 		case 6:
+	// 			print("You picked" + num );
+	// 		break;
 
-			case 10:
-				print("You picked" + num );
-			break;
-			
-			default:
-				print("I don't understand" + num);
-			break;
-		}
+	// 		case 10:
+	// 			print("You picked" + num );
+	// 		break;
+
+	// 		default:
+	// 			print("I don't understand" + num);
+	// 		break;
+	// 	}
+
+	switch(name){
+		case "Jason":
+			print("Welcome to camp crystal lake" + name + " - love mother");
+		break;
+
+		case "Michael":
+			print("Welcome to Haddonfield, Ill" + name);
+		break;
+
+		case "Freddie":
+			print("Welcome to Elm Street" = name);
+		break;
+
+		case "Leatherface":
+			print("The stars and stripes are big at night...Deep in the heart of Texas");
+		break;
+
+		default:
+			print("I don't know" + name);
+		break;
+		
+	}
 	}
 	
 	// Update is called once per frame
