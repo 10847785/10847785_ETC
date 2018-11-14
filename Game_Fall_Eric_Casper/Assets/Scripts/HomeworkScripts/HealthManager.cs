8956336f4dd1d53765cf984cf96dc395945d5464
Eric Casper <eric322000@gmail.com>
2018-11-14 16:50:47 -0700

    Added code to HealthManager.

--- Game_Fall_Eric_Casper/Assets/Scripts/HomeworkScripts/HealthManager.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/HomeworkScripts/HealthManager.cs
@@ -6,13 +6,24 @@ public class HealthManager : MonoBehaviour {
 
 	public static int Health;
 
+	Text HealthText;
+
 	// Use this for initialization
 	void Start () {
+		HealthText = GetComponent<Text>();
+
+		Health = 10;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Health > 10)
+			Health = 10;
+		HealthText.text = " " + Health;
+	}
+
+	public static void AddHealth (int HealthToAdd) {
+		Health += HealthToAdd;
 	}
 }
