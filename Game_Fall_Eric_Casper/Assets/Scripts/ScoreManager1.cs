e03a54431df577b522818319f7e70ed48159a516
Eric Casper <eric322000@gmail.com>
2018-11-28 18:24:32 -0700

    Endgame has been added.

--- Game_Fall_Eric_Casper/Assets/Scripts/ScoreManager1.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/ScoreManager1.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager1 : MonoBehaviour {
 	
 	public static int Score;
+	public int WinScore;
+
+	public Text WinText;
 
 	Text ScoreText;
+
+	void Awake(){
+		Time.timeScale = 1;
+	}
 
 	// Use this for initialization
 	void Start () {
 		ScoreText = GetComponent<Text>();
 
 		Score = 0;
+
+		WinText.GetComponent<Text>().enabled = false;
 	}
 
 	// Update is called once per frame
@@ -21,6 +31,18 @@ public class ScoreManager1 : MonoBehaviour {
 		if (Score < 0)
 			Score = 0;
 		ScoreText.text = " " + Score;
+
+		// If the player win display win test
+		if(Score >= WinScore ){
+			print("Win Score Reached =" + Score);
+			WinText.GetComponent<Text>().enabled = true;
+			Time.timeScale = 0;
+		}
+		// If player hits the Escape key return to start menu
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene(0);
+		}
+
 	}
 
 	public static void AddPoints (int PointsToAdd) {
