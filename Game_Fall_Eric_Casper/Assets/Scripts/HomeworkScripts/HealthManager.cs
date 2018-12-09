d5244931106169f45b4433be6424f31a0c806852
Eric Casper <eric322000@gmail.com>
2018-12-09 09:02:45 -0700

    Health can go down again

--- Game_Fall_Eric_Casper/Assets/Scripts/HomeworkScripts/HealthManager.cs
+++ Game_Fall_Eric_Casper/Assets/Scripts/HomeworkScripts/HealthManager.cs
@@ -2,28 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour {
 
 	public static int Health;
-	// public int GameOver;
+	public int LoseScore;
+	public Text LoseText;
 	Text HealthText;
-	// public Text GameOver;
 	
+	void Awake() {
+		Time.timeScale = 1;
+	}
 
 	// Use this for initialization
 	void Start () {
 		HealthText = GetComponent<Text>();
 
-		Health = 10;
-		// HealthText.GetComponent<Text>().enabled = false;
+		Health = 2;
+		LoseText.GetComponent<Text>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Health < 10)
-			Health = 10;
+		if (Health < 2)
+			Health = 2;
 		HealthText.text = " " + Health;
+
+		// If the player win display win test
+		if(Health >= LoseScore ){
+			print("Game Over =" + Health);
+			LoseText.GetComponent<Text>().enabled = true;
+			Time.timeScale = 0;
+		}
+		// If player hits the Escape key return to start menu
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene(0);
+		}
 	}
 
 	public static void AddHealth (int HealthToAdd) {
